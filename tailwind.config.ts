4a9753e5dc6f3b52b11b2957e7dd290a06196f8d
Zarqan <zarqannazeer4567@gmail.com>
2025-05-08 14:43:08 +0530

    df

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./client/index.html",
+    "./client/src/**/*.{js,jsx,ts,tsx}",
+    "./shared/**/*.{js,jsx,ts,tsx}", // <-- add this
+  ],
+  
   theme: {
     extend: {
       fontFamily: {
